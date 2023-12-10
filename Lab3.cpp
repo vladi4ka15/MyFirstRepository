5a2764b4c1b8c875291fee60967b908c9273a78c
unknown <blidarv871@gmail.com>
2023-12-10 16:38:40 +0200

    First Commit

---
+++ Lab3.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b;
+
+    cout << "Введіть число A: ";
+    cin >> a;
+
+    cout << "Введіть число B: ";
+    cin >> b;
+    cout << boolalpha << "Результат: " << ((a % 2 != 0 && b % 2 == 0) || (a % 2 == 0 && b % 2 != 0));
+}
